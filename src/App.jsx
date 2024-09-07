81cfcc6268bb0f1bd19dbd3ff7b0b946d17c2e0a
Doniyor-Roziqulov <doniyorruziqulov33@gmail.com>
2024-09-07 23:11:41 +0500

    First coomit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CreateUser from "./create-user/CreateUser";
+
+function App() {
+    return (
+        <>
+            <CreateUser />
+        </>
+    );
+}
+
+export default App;
---
+++ src/redux/user-slice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const usersSlice = createSlice({
+    name: "users",
+    initialState: {
+        value: [],
+    },
+    reducers: {
+        addUsers(state, action) {
+            state.value = [...state.value, action.payload];
+        },
+        removeUsers(state, action) {
+            state.value = state.value.filter(
+                (user) => user.id !== action.payload.id
+            );
+        },
+    },
+});
+
+export const { addUsers, removeUsers } = usersSlice.actions;
+export default usersSlice.reducer;
